03603bf39c6b9607b99d15a41b264881bb7a2fa8
Clairessa M. Brown <brow4011@umn.edu>
2019-09-12 13:45:03 -0700

    Followed pipeline tutorial

--- configuration.py
+++ configuration.py
@@ -4,7 +4,7 @@
 """
 
 # Setup the name of our extractor.
-EXTRACTOR_NAME = ""
+EXTRACTOR_NAME = "droner"
 
 # Name of scientific method for this extractor. Leave commented out if it's unknown
 #METHOD_NAME = ""
@@ -13,16 +13,16 @@ EXTRACTOR_NAME = ""
 VERSION = "1.0"
 
 # The extractor description
-DESCRIPTION = ""
+DESCRIPTION = "Extracts greenness or other RGB indices from digital images"
 
 # The name of the author of the extractor
-AUTHOR_NAME = ""
+AUTHOR_NAME = "Clairessa Brown"
 
 # The email of the author of the extractor
-AUTHOR_EMAIL = ""
+AUTHOR_EMAIL = "clairessabrown@email.arizona.edu"
 
 # Reposity URI
-REPOSITORY = ""
+REPOSITORY = "github.com/cmbrown89/drone-extractor2"
 
 # Output variable identifiers. Use a comma separated list if more than one value is returned.
 # For example, "variable 1,variable 2" identifies the two variables returned by the extractor.
@@ -30,4 +30,4 @@ REPOSITORY = ""
 # Note that variable names cannot have comma's in them: use a different separator instead. Also,
 # all white space is kept intact; don't add any extra whitespace since it may cause name comparisons
 # to fail
-VARIABLE_NAMES = ""
+VARIABLE_NAMES = "veg_ind,exg,per_green"
--- extractor.py
+++ extractor.py
@@ -9,5 +9,19 @@ def calculate(pxarray):
         pxarray(numpy array): RGB pixel data at the plot level
     Returns:
     """
-    # Replace this following line with your algorithm
-    return pxarray[:, :, 1].size  # Returning the size of a single color channel in pixels
+
+    # Extract the mean red, green, and blue values from the image bands
+    r = pxarray[:,:,0].mean()
+    g = pxarray[:,:,1].mean()
+    b = pxarray[:,:,2].mean()   
+    
+    a = 0.667 # Veg index
+
+    # Formulas
+    veg_ind = g/(r**a*b**(1-a))
+    exg = 2*g-(r+b)
+    per_green = g/(r+b+g)
+
+    indices = [veg_ind, exg, per_green]
+
+    return indices
